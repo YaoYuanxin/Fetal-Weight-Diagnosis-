dc50abb8f20a5717edf440724440542e66c923db
YaoYuanxin <yaoyuanxin2017@gmail.com>
2022-12-02 05:33:37 +0000

    more fixes

--- test.py
+++ test.py
@@ -1,25 +1,14 @@
 """
 test
 """
-
+import io
 import pandas as pd
 import requests
-import io
 
-# Username of your GitHub account
 
-username = 'YaoYuanxin'
-
-# Personal Access Token (PAO) from your GitHub account
-
-token = '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'
-
-# Creates a re-usable session object with your creds in-built
-
-    
 # Downloading the csv file from your GitHub
 
-url = "https://raw.githubusercontent.com/YaoYuanxin/remote_private_data/main/penguins_cleaned.csv?token=GHSAT0AAAAAAB2W6ITGKCT2LXWMMM7W7O5SY4JCAYA"
+url = "https://raw.githubusercontent.com/YaoYuanxin/remote_private_data/main/penguins_cleaned.csv?token=GHSAT0AAAAAAB2W6ITHGLEVOSBZVDXGLHGGY4JFV7A"
 download = requests.get(url).content
 
 # Reading the downloaded content and making it a pandas dataframe
